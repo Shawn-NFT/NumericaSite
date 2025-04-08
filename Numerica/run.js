753e7d107a0a51c60cc979f94a60744467a1b987
Shawn-NFT <shawnnft76@gmail.com>
2025-04-08 14:04:43 -0700

    Delete Numerica directory

--- Numerica/run.js
+++
@@ -1,9 +0,0 @@
-function rnd(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-console.log(10**500)
-
-console.log(rnd(1, 1*10**271))
